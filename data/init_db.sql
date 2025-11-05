412b24dc1f8c706a8fbf34223540b57fad124a71
id2733 <clemence.pinsard@eleve.ensai.fr>
2025-11-05 15:21:52 +0000

    table

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,6 +1,8 @@
 -- Supprime les tables si elles existent déjà (utile pour réinitialiser la base)
 DROP TABLE IF EXISTS joueurs CASCADE;
 DROP TABLE IF EXISTS joueurs_statistiques CASCADE;
+DROP TABLE IF EXISTS table_partie CASCADE;
+DROP TABLE IF EXISTS table_joueur CASCADE;
 
 
 -- Table des joueurs (infos administratives)
@@ -28,3 +30,21 @@ CREATE TABLE joueurs_statistiques (
     nombre_parties_dernier_mois INT DEFAULT 0,
     FOREIGN KEY (pseudo) REFERENCES joueurs(pseudo) ON DELETE CASCADE
 );
+
+-- Table Partie (correspond à la table où se déroule la partie)
+CREATE TABLE table_partie (
+    id SERIAL PRIMARY KEY,
+    blind INTEGER DEFAULT 10 CHECK (blind >= 0),
+    pot INTEGER DEFAULT 0 CHECK (pot >= 0),
+    indice_dealer INTEGER NOT NULL DEFAULT 0,
+    deck JSONB NOT NULL,
+    board JSONB NOT NULL
+);
+
+-- Table joueur fait le lien entre la table et les joueurs
+CREATE TABLE table_joueur (
+    table_id INTEGER REFERENCES table_partie(id) ON DELETE CASCADE,
+    joueur_id INTEGER REFERENCES joueurs(id) ON DELETE CASCADE,
+    PRIMARY KEY (table_id, joueur_id)
+);
+
---
+++ data/pop_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO table_partie (id, blind, pot, indice_dealer, deck, board) VALUES
+(1, 10, 0, 0, NULL, NULL),
+(2, 10, 0, 0, NULL, NULL),
+(3, 10, 0, 0, NULL, NULL),
+(4, 10, 0, 0, NULL, NULL),
+(5, 10, 0, 0, NULL, NULL),
+(6, 10, 0, 0, NULL, NULL),
+(7, 10, 0, 0, NULL, NULL),
+(8, 10, 0, 0, NULL, NULL),
+(9, 10, 0, 0, NULL, NULL),
+(10, 10, 0, 0, NULL, NULL);
