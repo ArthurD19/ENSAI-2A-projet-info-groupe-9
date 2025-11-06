7cf9479aa5f40e7a3a4f88de1301214b599a93e9
arthurdeletang <arthur.deletang@eleve.ensai.fr>
2025-11-06 11:39:46 +0000

    mdp

--- src/view/accueil/inscription_vue.py
+++ src/view/accueil/inscription_vue.py
@@ -1,9 +1,6 @@
 import os
-
-import regex
 from InquirerPy import inquirer
-from InquirerPy.validator import EmptyInputValidator, PasswordValidator
-from prompt_toolkit.validation import ValidationError, Validator
+from InquirerPy.validator import PasswordValidator
 
 from service.joueur_service import JoueurService
 from view.vue_abstraite import VueAbstraite
@@ -16,46 +13,52 @@ class InscriptionVue(VueAbstraite):
         self.tables = tables
 
     def choisir_menu(self):
-        length = int(os.environ.get("PASSWORD_LENGTH", 16))
-        # Demande à l'utilisateur de saisir pseudo, mot de passe...
-        pseudo = inquirer.text(message="Entrez votre pseudo : ").execute()
+        """Demande à l'utilisateur pseudo, mot de passe et code de parrainage, puis crée le joueur"""
 
-        if JoueurService().pseudo_deja_utilise(pseudo):
-            from view.accueil.accueil_vue import AccueilVue
+        # Récupérer la longueur de mot de passe minimale depuis l'environnement
+        length = int(os.environ.get("PASSWORD_LENGTH", 8))
 
-            return AccueilVue(f"Le pseudo {pseudo} est déjà utilisé.", self.tables)
-
-        mdp = inquirer.secret(
-            message="Entrez votre mot de passe, il doit contenir au moins 8 caractères, une majuscule et un chiffre: ",
-            validate=PasswordValidator(
-                length,
-                cap=True,
-                number=True,
-            ),
+        # Demande du pseudo
+        pseudo = inquirer.text(
+            message="Entrez votre pseudo : "
         ).execute()
 
-        code_de_parrainage = inquirer.text(message="Entrez un code de parrainage : ").execute()
+        # Vérifier que le pseudo n'est pas déjà utilisé
+        if JoueurService().pseudo_deja_utilise(pseudo):
+            from view.accueil.accueil_vue import AccueilVue
+            return AccueilVue(f"Le pseudo '{pseudo}' est déjà utilisé.", self.tables)
 
-        if code_de_parrainage != "":
+        # Demande du mot de passe avec validation
+        mdp = inquirer.secret(
+            message=f"Entrez votre mot de passe (au moins {length} caractères, 1 majuscule et 1 chiffre) : ",
+            validate=PasswordValidator(
+                length=length,
+                cap=True,
+                number=True,
+                message=f"Le mot de passe doit contenir au moins {length} caractères, 1 majuscule et 1 chiffre."
+            )
+        ).execute()
+
+        # Demande du code de parrainage (facultatif)
+        code_de_parrainage = inquirer.text(
+            message="Entrez un code de parrainage (facultatif) : "
+        ).execute().strip()
+
+        # Création du joueur selon la présence ou non du code de parrainage
+        if code_de_parrainage:
             if JoueurService().code_valide(code_de_parrainage):
                 joueur = JoueurService().creer(pseudo, mdp, code_de_parrainage)
             else:
-                message = "Code de parrainage non valide."
                 from view.accueil.accueil_vue import AccueilVue
-                return AccueilVue(message, self.tables)
-
+                return AccueilVue("Code de parrainage non valide.", self.tables)
         else:
             joueur = JoueurService().creer_sans_code_parrainage(pseudo, mdp)
 
-        # Si le joueur a été créé
-        if joueur is not None:
-            message = (
-                f"Votre compte {joueur['pseudo']} a été créé. Vous pouvez maintenant vous connecter."
-            )
+        # Vérifier si le joueur a été créé
+        if joueur:
+            message = f"Votre compte '{joueur['pseudo']}' a été créé. Vous pouvez maintenant vous connecter."
         else:
-            message = "Erreur de connexion (pseudo ou mot de passe invalide)"
+            message = "Erreur lors de la création du compte. Vérifiez vos informations."
 
         from view.accueil.accueil_vue import AccueilVue
-
         return AccueilVue(message, self.tables)
-
