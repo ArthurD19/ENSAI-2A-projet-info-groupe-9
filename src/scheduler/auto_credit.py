84086b904d40bdbcacc73cb5228347eae3e44efc
id2733 <clemence.pinsard@eleve.ensai.fr>
2025-11-20 14:31:28 +0000

    modif heure

--- src/scheduler/auto_credit.py
+++ src/scheduler/auto_credit.py
@@ -1,13 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from src.service.joueur_service import JoueurService
-
+import pytz
 
 def lancer_auto_credit():
     """
     Fonction qui automatise le rechargement des portefeuilles de façon régulière.
     """
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone=pytz.timezone("Europe/Paris"))
     JoueurService().credit_auto()
-    scheduler.add_job(JoueurService().credit_auto, "cron", hour=3, minute=0)
+    scheduler.add_job(JoueurService().credit_auto, "cron", hour=15, minute=30)
     scheduler.start()
